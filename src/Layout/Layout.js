f9259d2ce17c1ea3699c4e671b9449174bdcabc9
Tim Rohweder <info@portlandcarrot.com>
2019-02-02 19:03:27 -0800

    Add missing files from last commit

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -4,15 +4,15 @@ import styled from 'styled-components';
 import Header from './Header';
 
 const Container = styled.div`
-  --header-height: 10rem;
-  display: grid;
-  grid-template-columns: var(--cols);
-  grid-template-rows: var(--header-height) min-content 1fr;
-`
+    --header-height: 10rem;
+    display: grid;
+    grid-template-columns: var(--cols);
+    grid-template-rows: var(--header-height) min-content 1fr;
+`;
 
-export default (props) => (
-  <Container>
-    <Header />
-    {props.children}
-  </Container>
+export default props => (
+    <Container>
+        <Header />
+        {props.children}
+    </Container>
 );
